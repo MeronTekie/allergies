915386b4263d62c4e4c4d4141dc5930d87780b3c
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-03 15:17:37 -0700

    Create all necessary files for the project
    Co-authored-by: Marcus Lorenzo <marcus.kaitara.lorenzo@gmail.com>
    Co-authored-by: Meron G.Tekie <mtekiesd210309@gmail.com>

---
+++ ModelsTest/AllergiesTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Allergies;
+
+namespace Allergies.Tests
+{
+  [TestClass]
+  public class AllergiesTests
+  {
+    [TestMethod]
+    public void IsItAllergy()
+    {
+      
+    }
+  }
+}
